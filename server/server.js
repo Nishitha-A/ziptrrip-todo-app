82f0275a0b79f86c34cfbf757c887d34bb1850af
Nishitha <nishithaashwath05@example.com>
2026-09-01 23:56:10 +0530

    Add API root route

--- server/server.js
+++ server/server.js
@@ -10,6 +10,10 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.get("/", (req, res) => {
+  res.send("Ziptrrip Todo API is running successfully 🚀");
+});
+
 app.use("/api/todos", todoRoutes);
 
 mongoose

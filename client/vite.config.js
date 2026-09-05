bae3a441183c6879488f091ddfb42eb8c2477282
Nishitha <nishithaashwath05@example.com>
2026-09-05 14:21:17 +0530

    Fix deployed todo details page

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,6 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { resolve } from "path";
+import { fileURLToPath, URL } from "node:url";
 
 export default defineConfig({
   plugins: [react()],
@@ -17,8 +17,12 @@ export default defineConfig({
   build: {
     rollupOptions: {
       input: {
-        main: resolve(__dirname, "index.html"),
-        todo: resolve(__dirname, "todo.html"),
+        main: fileURLToPath(
+          new URL("./index.html", import.meta.url)
+        ),
+        todo: fileURLToPath(
+          new URL("./todo.html", import.meta.url)
+        ),
       },
     },
   },
